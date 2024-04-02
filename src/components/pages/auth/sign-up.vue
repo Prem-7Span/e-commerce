d5c4c473e87df34f222de14955f38651694ede7b
prem-7Span <prem.t@7span.com>
2024-04-02 17:12:16 +0530

    Folder structure & Pages update

---
+++ src/components/pages/auth/sign-up.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="flex">
+    <div><img src="../assets/bg-signup.png" alt="background-image" /></div>
+    <div class="flex items-center justify-center min-h-screen bg-black">
+      <div class="w-full max-w-md p-8 bg-white rounded-lg shadow-xl">
+        <h2 class="mb-8 text-3xl font-extrabold text-center text-gray-900">
+          Sign up
+        </h2>
+        <form @submit.prevent="submitForm">
+          <div class="space-y-4">
+            <div class="flex flex-col sm:flex-row sm:space-x-8">
+              <div>
+                <input
+                  v-model="firstname"
+                  type="text"
+                  placeholder="First Name"
+                  class="w-full px-4 py-2 bg-gray-100 rounded-md sm:w-44"
+                />
+
+                <p class="mt-2 text-red-500">{{ errors.firstname }}</p>
+              </div>
+              <div>
+                <input
+                  v-model="lastname"
+                  type="text"
+                  placeholder="Last Name"
+                  class="w-full px-4 py-2 bg-gray-100 rounded-md sm:w-44"
+                />
+                <p class="mt-2 text-red-500">{{ errors.lastname }}</p>
+              </div>
+            </div>
+            <div>
+              <input
+                v-model="email"
+                type="email"
+                placeholder="Email"
+                class="w-full px-4 py-2 bg-gray-100 rounded-md focus:ring-purple-600"
+              />
+              <p class="mt-2 text-red-500">{{ errors.email }}</p>
+            </div>
+            <div>
+              <input
+                v-model="phoneNumber"
+                type="text"
+                maxlength="12"
+                placeholder="Phone Number"
+                class="w-full px-4 py-2 bg-gray-100 rounded-md"
+              />
+              <p class="mt-2 text-red-500">{{ errors.phoneNumber }}</p>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="w-full py-2 font-semibold text-white bg-blue-500 rounded-md"
+              >
+                Sign up
+              </button>
+            </div>
+            <div id="recaptcha-container"></div>
+            <p class="flex justify-end text-blue-500">
+              <router-link to="/signin">Sign In</router-link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import authentication from "/src/components/config.js";
+
+export default {
+  data() {
+    return {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phoneNumber: "",
+      recaptchaVerifier: undefined,
+      errors: {},
+    };
+  },
+  methods: {
+    generateCaptcha() {
+      console.log("Entering generateCaptcha method");
+      this.recaptchaVerifier = new RecaptchaVerifier(
+        authentication,
+        "recaptcha-container",
+        {
+          size: "invisible",
+          callback: (response) => {
+            console.log("Recaptcha response:", response);
+          },
+        }
+      );
+    },
+
+    async submitForm() {
+      if (!this.recaptchaVerifier) {
+        console.log("Generating Captcha...");
+        this.generateCaptcha();
+      } else {
+        try {
+          const phoneNumberVerification = await signInWithPhoneNumber(
+            authentication,
+            `+91${this.phoneNumber}`,
+            this.recaptchaVerifier
+          );
+
+          console.log(
+            "Phone number verification successful:",
+            phoneNumberVerification
+          );
+
+          this.$router.push({
+            name: "VerificationOtp",
+            query: { obj: phoneNumberVerification.verificationId },
+          });
+        } catch (error) {
+          console.error("Error during phone number verification:", error);
+          // Handle the error appropriately, e.g., display an error message to the user
+        }
+      }
+
+      this.errors = {};
+      if (!this.firstname) {
+        this.errors.firstname = "First Name is required";
+      }
+      if (!this.lastname) {
+        this.errors.lastname = "Last Name is required";
+      }
+      if (!this.email) {
+        this.errors.email = "Email is required";
+      } else if (!this.isValidEmail(this.email)) {
+        this.errors.email = "Invalid email address";
+      }
+      if (!this.phoneNumber) {
+        this.errors.phoneNumber = "Phone Number is required";
+      }
+
+      if (Object.keys(this.errors).length === 0) {
+        console.log(
+          "Form submitted successfully (client-side validation passed)"
+        );
+      }
+    },
+
+    isValidEmail(email) {
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    },
+  },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,29 +1,34 @@
-import { createRouter, createWebHashHistory } from 'vue-router'; // Import createRouter and createWebHashHistory from vue-router
-import SignUp from './components/SignUp.vue';
-import SignIn from './components/SignIn.vue';
-import VerificationOtp from './components/VerificationOtp.vue'; // Make sure to include the correct file extension (.vue)
-
+import { createRouter, createWebHashHistory } from "vue-router"; // Import createRouter and createWebHashHistory from vue-router
+import SignUp from "./components/pages/auth/sign-up.vue";
+import SignIn from "./components/pages/auth/sign-in.vue";
+import VerificationOtp from "./components/pages/auth/verification-otp.vue"; // Make sure to include the correct file extension (.vue)
+import Auth from "./components/layouts/auth.vue";
 const routes = [
-    {
-        name: "signUp",
-        path: '/',
-        component: SignUp
-    },
-    {
-        name: "signIn",
-        path: '/signin',
-        component: SignIn
-    },
-    {
-        name: "VerificationOtp",
-        path: '/otp', // Corrected path to '/otp'
-        component: VerificationOtp
-    }
+  {
+    name: "auth",
+    path: "/",
+    component: Auth,
+  },
+  {
+    name: "signUp",
+    path: "/signup",
+    component: SignUp,
+  },
+  {
+    name: "signIn",
+    path: "/signin",
+    component: SignIn,
+  },
+  {
+    name: "VerificationOtp",
+    path: "/otp", // Corrected path to '/otp'
+    component: VerificationOtp,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+  history: createWebHashHistory(),
+  routes,
 });
 
 export default router;
