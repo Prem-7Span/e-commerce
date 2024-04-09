99187af95b20eba94f202d77c6a4b26d4010816e
shubh-7Span <shubham.p@7span.com>
2024-04-09 18:20:48 +0530

    ui done

--- src/components/layouts/auth.vue
+++ src/components/layouts/auth.vue
@@ -1,15 +1,8 @@
 <template>
-  
-  <div id="main-container" class="grid grid-cols-2">
-    <div
-      id="container-1"
-      class="bg-[url('/src/assets/bg-signup.png')] bg-no-repeat h-screen max-w-fll max-h-full"
-    >
-    
-      <!-- <img src="/src/assets/bg-signup.png" alt="bg-img" /> -->
+  <div id="main-container" class="md:grid md:grid-cols-2 grid-cols-1">
+    <div id="container-1" class="md:bg-[url('/src/assets/bg-signup.png')] md:bg-no-repeat md:h-screen md:max-w-full md:max-h-full hidden lg:block">
       <div class="flex flex-col items-center justify-between h-full py-10">
-        <img src="/src/assets/Logo.svg" class="max-w-96" alt="" />
-
+        <img src="/src/assets/Logo.svg" class="md:max-w-96" alt="" />
         <h1 class="text-6xl text-white">Your fashion,your way!</h1>
       </div>
     </div>
