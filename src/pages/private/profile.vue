7baf51c21bb023b259b26a37ce5ae148d9656c50
Shubham Parsaniya <shubhparsaniya9@gmail.com>
2024-05-24 17:15:29 +0530

    bug fix

--- src/pages/private/profile.vue
+++ src/pages/private/profile.vue
@@ -34,8 +34,8 @@
       <div class="flex flex-col space-y-2">
         <div class="animate__animated animate__fadeIn">Address 1</div>
         <div v-if="userDetails.addresses && userDetails.addresses.length">
-          <h1 class="px-3 py-2 text-gray-700 transition duration-200 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-2 focus:ring-indigo-500 cursor-pointer">
-            {{ userDetails.addresses }}
+          <h1 class="px-3 py-6  text-gray-700 transition duration-200 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-2 focus:ring-indigo-500 cursor-pointer">
+            <!-- {{ userDetails.addresses }} -->
           </h1>
         </div>
         <div v-else class="text-gray-400">Add Your Address</div>
