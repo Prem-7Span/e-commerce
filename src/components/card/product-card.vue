60148f2f64ae450bb44a0a1fbbde18371743ce21
prem-7Span <prem.t@7span.com>
2024-06-13 12:36:58 +0530

    wishlist updated

--- src/components/card/product-card.vue
+++ src/components/card/product-card.vue
@@ -18,6 +18,7 @@
 
       <div id="wishlist">
         <button
+          v-if="$route.path !== '/wishlist'"
           @click.stop="toggleWishlist"
           class="absolute top-0 right-0 px-2 py-2 mt-2 mb-2 mr-2 bg-white rounded-full"
         >
