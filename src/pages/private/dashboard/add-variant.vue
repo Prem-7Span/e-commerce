d5487e1329cfc2dfe55cebab4c13bd7c3a7948c0
Shubham Parsaniya <shubham.p@7span.com>
2024-06-13 17:46:30 +0530

    imges bug fix

--- src/pages/private/dashboard/add-variant.vue
+++ src/pages/private/dashboard/add-variant.vue
@@ -1,33 +1,31 @@
 <template>
   <div class="p-4 px-0">
     <h1 class="text-2xl font-bold text-primary-300 mb-4">Add Variant</h1>
-    <form s class="space-y-4">
+    <form class="space-y-4">
       <div class="flex items-center space-x-4">
         <label for="size" class="text-lg mb-1 w-32">Size</label>
         <select id="size" v-model="newVariant.size" class="flex w-1/2 p-2 border rounded-md">
-          <option value="" disabled selected>Select</option>
-          <!-- Add options here -->
-          <option value="S">XS</option>
-          <option value="M">S</option>
-          <option value="L">M</option>
+          <option value="" disabled>Select</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
           <option value="L">L</option>
-          <option value="L">XL</option>
-          <option value="L">2XL</option>
-          <option value="L">3XL</option>
+          <option value="XL">XL</option>
+          <option value="2XL">2XL</option>
+          <option value="3XL">3XL</option>
         </select>
       </div>
 
       <div class="flex items-center space-x-4">
         <label for="Colors" class="text-lg mb-1 w-32">Colors</label>
         <select id="Colors" v-model="newVariant.color" class="flex w-1/2 p-2 border rounded-md">
-          <option value="" disabled selected>Select</option>
-          <!-- Add options here -->
-          <option value="S">Black</option>
-          <option value="M">White</option>
-          <option value="L">Blue</option>
-          <option value="L">Green</option>
-          <option value="L">Pink</option>
-          <option value="L">Red</option>
+          <option value="" disabled>Select</option>
+          <option value="Black">Black</option>
+          <option value="White">White</option>
+          <option value="Blue">Blue</option>
+          <option value="Green">Green</option>
+          <option value="Pink">Pink</option>
+          <option value="Red">Red</option>
         </select>
       </div>
 
@@ -37,8 +35,8 @@
       </div>
 
       <div class="flex items-center space-x-4">
-        <label for="currentPrice" class="text-lg mb-1 w-32">Current Price</label>
-        <input type="number" id="currentPrice" v-model="newVariant.currentPrice" class="flex w-1/2 p-2 border rounded-md">
+        <label for="price" class="text-lg mb-1 w-32"> Price</label>
+        <input type="number" id="price" v-model="newVariant.price" class="flex w-1/2 p-2 border rounded-md">
       </div>
 
       <div class="flex items-center space-x-4">
@@ -46,20 +44,41 @@
         <input type="number" id="stock" v-model="newVariant.stock" class="flex w-1/2 p-2 border rounded-md">
       </div>
 
+
+      <div class="flex items-center space-x-4">
+        <label for="stock" class="text-lg mb-1 w-32">AvailableQuantity</label>
+        <input type="number" id="stock" v-model="newVariant.availableQuantity" class="flex w-1/2 p-2 border rounded-md">
+      </div>
+
+      <div class="flex items-center space-x-4">
+        <label for="status" class="text-lg w-32">Primary</label>
+        <label class="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" v-model="newVariant.isDefault" id="isDefault" class="sr-only peer" />
+          <div
+            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary-300">
+          </div>
+          <span class="ml-2 text-lg font-medium text-gray-900"></span>
+        </label>
+      </div>
+
+
       <div class="flex space-x-4">
         <button type="button" @click="addNewVariant" class="p-2 bg-primary-300 text-white rounded-md">Add Variant</button>
       </div>
     </form>
 
-    <div class="mt-4">
+    <div class="mt-4 hidden">
       <h2 class="text-xl font-bold text-primary-300 mb-2">New Variants</h2>
       <ul>
         <li v-for="(variant, index) in variants" :key="index" class="mb-2 p-2 rounded-md border shadow-sm variant-item flex gap-4 px-3">
-          <span class="block text-lg"><strong >Color:</strong> {{ variant.color }}</span>
-          <span class="block text-lg"><strong >Size:</strong> {{ variant.size }}</span>
+          <span class="block text-lg"><strong>Color:</strong> {{ variant.color }}</span>
+          <span class="block text-lg"><strong>Size:</strong> {{ variant.size }}</span>
           <span class="block text-lg"><strong>Regular Price:</strong> {{ variant.regularPrice }}</span>
-          <span class="block text-lg"><strong>Current Price:</strong> {{ variant.currentPrice }}</span>
+          <span class="block text-lg"><strong>Price:</strong> {{ variant.price }}</span>
           <span class="block text-lg"><strong>Stock:</strong> {{ variant.stock }}</span>
+          <span class="block text-lg"><strong>Stock:</strong> {{ variant.availableQuantity }}</span>
+          <span class="block text-lg"><strong>Stock:</strong> {{ variant.isDefault }}</span>
+
         </li>
       </ul>
     </div>
@@ -67,28 +86,36 @@
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue';
+import { reactive, ref, defineEmits } from 'vue';
+
+const emit = defineEmits(['variantAdded']);
 
 const newVariant = reactive({
   size: '',
-  color:'',
+  color: '',
   regularPrice: '',
-  currentPrice: '',
-  stock: ''
+  price: '',
+  stock: '',
+  availableQuantity:'',
+  isDefault: false
 });
+
 const variants = ref([]);
 
 const addNewVariant = () => {
-  if (newVariant.size && newVariant.regularPrice && newVariant.currentPrice && newVariant.stock) {
-    variants.value.push({ ...newVariant });
+  if (newVariant.size && newVariant.regularPrice && newVariant.price && newVariant.stock && newVariant.availableQuantity && newVariant.isDefault) {
+    const variant = { ...newVariant };
+    variants.value.push(variant);
+    emit('variantAdded', variant);
     newVariant.size = '';
-    newVariant.color = '',
+    newVariant.color = '';
     newVariant.regularPrice = '';
-    newVariant.currentPrice = '';
+    newVariant.price = '';
     newVariant.stock = '';
+    newVariant.availableQuantity ='';
+    newVariant.isDefault= false
   } else {
     alert('Please fill in all fields.');
   }
 };
 </script>
-
