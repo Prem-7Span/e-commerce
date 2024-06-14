12fff6771cde89dc54c59cc296e4ce1b094b9af8
Shubham Parsaniya <shubham.p@7span.com>
2024-06-14 15:47:37 +0530

    addNewVariant bug fix

--- src/pages/private/dashboard/add-variant.vue
+++ src/pages/private/dashboard/add-variant.vue
@@ -11,8 +11,8 @@
           <option value="M">M</option>
           <option value="L">L</option>
           <option value="XL">XL</option>
-          <option value="2XL">2XL</option>
-          <option value="3XL">3XL</option>
+          <option value="XXL">XXL</option>
+          <option value="XXXL">XXXL</option>
         </select>
       </div>
 
@@ -44,23 +44,21 @@
         <input type="number" id="stock" v-model="newVariant.stock" class="flex w-1/2 p-2 border rounded-md">
       </div>
 
-
       <div class="flex items-center space-x-4">
-        <label for="stock" class="text-lg mb-1 w-32">AvailableQuantity</label>
-        <input type="number" id="stock" v-model="newVariant.availableQuantity" class="flex w-1/2 p-2 border rounded-md">
+        <label for="availableQuantity" class="text-lg mb-1 w-32">AvailableQuantity</label>
+        <input type="number" id="availableQuantity" v-model="newVariant.availableQuantity" class="flex w-1/2 p-2 border rounded-md">
       </div>
 
       <div class="flex items-center space-x-4">
-        <label for="status" class="text-lg w-32">Primary</label>
+        <label for="isDefault" class="text-lg w-32">Primary</label>
         <label class="relative inline-flex items-center cursor-pointer">
-          <input type="checkbox" v-model="newVariant.isDefault" id="isDefault" class="sr-only peer" />
+          <input type="checkbox" v-model="newVariant.isDefault" :disabled="isDefaultDisabled" id="isDefault" class="sr-only peer" />
           <div
             class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary-300">
           </div>
           <span class="ml-2 text-lg font-medium text-gray-900"></span>
         </label>
       </div>
-
 
       <div class="flex space-x-4">
         <button
@@ -82,9 +80,8 @@
           <span class="block text-lg"><strong>Regular Price:</strong> {{ variant.regularPrice }}</span>
           <span class="block text-lg"><strong>Price:</strong> {{ variant.price }}</span>
           <span class="block text-lg"><strong>Stock:</strong> {{ variant.stock }}</span>
-          <span class="block text-lg"><strong>Stock:</strong> {{ variant.availableQuantity }}</span>
-          <span class="block text-lg"><strong>Stock:</strong> {{ variant.isDefault }}</span>
-
+          <span class="block text-lg"><strong>Available Quantity:</strong> {{ variant.availableQuantity }}</span>
+          <span class="block text-lg"><strong>Primary:</strong> {{ variant.isDefault }}</span>
         </li>
       </ul>
     </div>
@@ -102,14 +99,18 @@ const newVariant = reactive({
   regularPrice: '',
   price: '',
   stock: '',
-  availableQuantity:'',
+  availableQuantity: '',
   isDefault: false
 });
 
 const variants = ref([]);
+const isDefaultDisabled = ref(false);
 
 const addNewVariant = () => {
-  if (newVariant.size && newVariant.regularPrice && newVariant.price && newVariant.stock && newVariant.availableQuantity && newVariant.isDefault) {
+  if (newVariant.size && newVariant.regularPrice && newVariant.price && newVariant.stock && newVariant.availableQuantity) {
+    if (newVariant.isDefault) {
+      isDefaultDisabled.value = true;
+    }
     const variant = { ...newVariant };
     variants.value.push(variant);
     emit('variantAdded', variant);
@@ -118,8 +119,8 @@ const addNewVariant = () => {
     newVariant.regularPrice = '';
     newVariant.price = '';
     newVariant.stock = '';
-    newVariant.availableQuantity ='';
-    newVariant.isDefault= false
+    newVariant.availableQuantity = '';
+    newVariant.isDefault = false;
   } else {
     alert('Please fill in all fields.');
   }
